db3e5f622037390db484349ab69955adef6aac76
DouglasThorkell <douglas.thorkell@icloud.com>
2024-12-08 22:59:18 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
+# Download SQLite source code
+wget https://www.sqlite.org/2023/sqlite-autoconf-3430100.tar.gz
+tar xzf sqlite-autoconf-3430100.tar.gz
+cd sqlite-autoconf-3430100
 
-# Set up SQLite build directory
-SQLITE_VERSION=3430100
-SQLITE_NAME="sqlite-autoconf-${SQLITE_VERSION}"
-INSTALL_DIR="$HOME/.local"
-
-# Download and extract SQLite
-wget "https://www.sqlite.org/2023/${SQLITE_NAME}.tar.gz"
-tar -xvf "${SQLITE_NAME}.tar.gz"
-cd "${SQLITE_NAME}"
-
-# Build and install SQLite locally
-./configure --prefix="${INSTALL_DIR}"
+# Configure and build SQLite
+./configure --prefix=$HOME/.local
 make
 make install
 
-# Update PATH to prioritize locally installed SQLite
-export PATH="${INSTALL_DIR}/bin:$PATH"
-
-# Verify the installed version
-sqlite3 --version
+# Verify SQLite build
+$HOME/.local/bin/sqlite3 --version
